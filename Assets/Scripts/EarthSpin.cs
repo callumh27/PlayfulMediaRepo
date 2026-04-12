3cd03339f50070242d5f6d26668d5400023f02b7
brafc <brad.chapman24@bathspa.ac.uk>
2026-04-12 23:08:06 +0100

    fixed Earth's spin again

--- Assets/Scripts/EarthSpin.cs
+++ Assets/Scripts/EarthSpin.cs
@@ -6,6 +6,6 @@ public class EarthSpin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0.1f, 0), Space.Self);
+        transform.Rotate(new Vector3(0,1,0), 0.1f);
     }
 }
