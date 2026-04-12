7f555e77bfdbb3d981731af521332f634c54d889
brafc <brad.chapman24@bathspa.ac.uk>
2026-04-12 23:04:06 +0100

    fixed earth's rotation axis

--- Assets/Scripts/EarthSpin.cs
+++ Assets/Scripts/EarthSpin.cs
@@ -6,6 +6,6 @@ public class EarthSpin : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0.1f, 0);
+        transform.Rotate(new Vector3(0, 0.1f, 0), Space.Self);
     }
 }
