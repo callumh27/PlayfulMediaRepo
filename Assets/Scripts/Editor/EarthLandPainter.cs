daa1f36c7442ce53047b69c76c49727c5c6372fd
brafc <brad.chapman24@bathspa.ac.uk>
2026-04-19 20:04:10 +0100

    modified terrain properties

--- Assets/Scripts/Editor/EarthLandPainter.cs
+++ Assets/Scripts/Editor/EarthLandPainter.cs
@@ -1,16 +1,8 @@
+using UnityEditor;
 using UnityEngine;
 
-public class EarthLandPainter : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class EarthLandPainter : Editor
+{
+    
 }
