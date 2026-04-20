892d510de8f6f1a55348a657705044e207ad0ece
brafc <brad.chapman24@bathspa.ac.uk>
2026-04-20 14:25:09 +0100

    Basic working painter tool
    still doesn't draw to texture

--- Assets/Scripts/EarthLandPainter.cs
+++ Assets/Scripts/EarthLandPainter.cs
@@ -1,4 +1,6 @@
 
+using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,14 +18,20 @@ public class EarthLandPainter : MonoBehaviour
     [Header("Properties")]
     [SerializeField] private Camera mainCamera;
     [SerializeField] private LayerMask earthLayer;
+    public int renderTextureSize = 256;
 
     public Vector3 position = Vector3.zero;
 
     public RenderTexture renderTexture;
+    public Texture3D texture;
 
     private void Update()
     {
         if (!isEnabled) return;
+
+       
+
+
         Vector3 mouseScreenPosition = new Vector3(Mouse.current.position.x.ReadValue(), Mouse.current.position.y.ReadValue(), 0);
 
         
@@ -33,8 +41,54 @@ public class EarthLandPainter : MonoBehaviour
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, earthLayer))
         {
             position = hit.point;
+            if (Mouse.current.leftButton.IsPressed())
+            {
+                float rSquared = radius * radius;
+
+                for (int u = (int)(hit.point.x - radius); u < (int)(hit.point.x + radius) + 1; u++)
+                {
+                    for (int v = (int)(hit.point.y - radius); v < (int)(hit.point.y + radius) + 1; v++)
+                    {
+                        for (int w = (int)(hit.point.z - radius); w < (int)(hit.point.z + radius) + 1; w++)
+                        {
+                            if ((hit.point.x - u) * (hit.point.x - u) + (hit.point.y - v) * (hit.point.y - v) + (hit.point.z - w) * (hit.point.z - w) < rSquared) {
+                                Vector3 convertedWorldPos = new Vector3(u - 0.5f, v - 0.5f, w - 0.5f) / (renderTextureSize - 1.0f);
+                                Vector3 texturePos = convertedWorldPos * 10;
+                                texture.SetPixel((int)texturePos.x, (int)texturePos.y, (int)texturePos.z, new Color(0.0f, 0.0f, 0.0f, 1.0f));
+                            }
+                        }
+                    }
+                }
+                texture.Apply();
+            }
+            
+                        
         }
 
+
+    }
+
+    public void CreateNewRenderTexture()
+    {
+       
+        texture = new Texture3D(renderTextureSize, renderTextureSize, renderTextureSize, TextureFormat.RGBA32, false);
+        
+
+    }
+
+    public void SaveRenderTexture()
+    {
+        RenderTexture newRenderTexture = new RenderTexture(renderTextureSize, renderTextureSize, 0);
+        newRenderTexture.enableRandomWrite = true;
+        newRenderTexture.graphicsFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.R32G32B32A32_UInt;
+        newRenderTexture.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
+        newRenderTexture.volumeDepth = renderTextureSize;
+        newRenderTexture.filterMode = FilterMode.Point;
+        newRenderTexture.Create();
+        renderTexture = newRenderTexture;
+
+        Graphics.CopyTexture(texture, newRenderTexture);
+        AssetDatabase.CreateAsset(renderTexture, "Assets/" + "EarthRenderTexture" + ".asset");
 
     }
 
---
+++ Assets/Scripts/EarthPainterInspector.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(EarthLandPainter))]
+public class EarthPainterInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        EarthLandPainter painter = (EarthLandPainter)target;
+        if (GUILayout.Button("Create New Earth Map"))
+        {
+            painter.CreateNewRenderTexture();
+        }
+        if (GUILayout.Button("Save Current Earth Map"))
+        {
+            painter.SaveRenderTexture();
+        }
+    }
+}
