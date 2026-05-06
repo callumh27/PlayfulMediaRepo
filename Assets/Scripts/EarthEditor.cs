d7e1538ae8c856ca22a084a0669a33f08c4c813c
brafc <BRAD.CHAPMAN24@bathspa.ac.uk>
2026-05-06 18:49:51 +0100

    created earth editor object

---
+++ Assets/Scripts/EarthEditor.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EarthEditor : MonoBehaviour
+{
+
+    public EarthTimelinePoint currentTimelineToEdit;
+    public Material earthMaterial;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        earthMaterial.SetTexture("_TectonicTexture", currentTimelineToEdit.tectonicMap);
+        earthMaterial.SetTexture("_LandmassTexture", currentTimelineToEdit.landmassMap;)
+    }
+}
--- Assets/Scripts/EarthSpin.cs
+++ Assets/Scripts/EarthSpin.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class EarthSpin : MonoBehaviour
 {
+    // user should be able to drag on the screen (if they arent clicking the planet)
+    // should have a delay to the drag
+
+
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0,1,0), 0.1f);
+        //transform.Rotate(new Vector3(0,1,0), 0.1f);
+        float xChange = Input.mousePositionDelta.x;
+        transform.Rotate(new Vector3(0,xChange,0), 0.1f);
     }
 }
