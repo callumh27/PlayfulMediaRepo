b4b6f14513bb35d8a70c0de749afcfa6b618bd05
brafc <BRAD.CHAPMAN24@bathspa.ac.uk>
2026-05-12 20:31:58 +0100

    implemented rotating the earth with mouse
    still is quite buggy

--- Assets/Scripts/EarthSpin.cs
+++ Assets/Scripts/EarthSpin.cs
@@ -1,11 +1,54 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EarthSpin : MonoBehaviour
 {
 
+    [SerializeField] float dragStrength = 1f;
+
+
+    private Vector3 rotationalVelocity = Vector3.zero;
+    private bool isMouseDown = false;
+
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0,1,0), 0.1f);
+        if (Input.GetMouseButtonUp(0))
+        {
+            float xDelta = Input.mousePositionDelta.x;
+            float yDelta = Input.mousePositionDelta.y;
+            rotationalVelocity = new Vector3(yDelta,-xDelta);
+            Debug.Log(rotationalVelocity);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            isMouseDown = true;
+            float xDelta = Input.mousePositionDelta.x;
+            float yDelta = Input.mousePositionDelta.y;
+            //rotationalVelocity = new Vector3(yDelta,-xDelta);
+            transform.Rotate(new Vector3(0,1,0), xDelta * -dragStrength, Space.Self);
+            //if (transform.rotation.eulerAngles.x < 25 && transform.eulerAngles.x > -25)
+            //{
+                transform.Rotate(new Vector3(1,0,0), yDelta * dragStrength, Space.World);
+            //}
+            
+            
+        }
+        transform.Rotate(new Vector3(0,1,0), 0.1f, Space.Self);
+        
     }
+
+    void FixedUpdate()
+    {
+        //rotational velocity decay
+        if (rotationalVelocity.magnitude < 1) return;
+
+        rotationalVelocity = Vector3.Lerp(rotationalVelocity, Vector3.zero, 0.1f);
+        float xVelocity = rotationalVelocity.x;
+        float yVelocity = rotationalVelocity.y;
+        transform.Rotate(new Vector3(xVelocity,0,0), Space.World);
+        transform.Rotate(new Vector3(0,yVelocity,0), Space.Self);
+    }
+
 }
