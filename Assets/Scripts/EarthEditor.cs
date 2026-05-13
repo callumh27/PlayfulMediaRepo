8b7bf86eda993be237e7d3ad65c31029de68cf10
brafc <BRAD.CHAPMAN24@bathspa.ac.uk>
2026-05-13 17:46:00 +0100

    updated earth editor

--- Assets/Scripts/EarthEditor.cs
+++ Assets/Scripts/EarthEditor.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
 
+public enum EarthEditorState
+{
+    None,
+    Landmass,
+    Tectonics
+}
+
 public class EarthEditor : MonoBehaviour
 {
+    public EarthEditorState currentEditingState;
+
 
     public EarthTimelinePoint currentTimelineToEdit;
     public Material earthMaterial;
+
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,5 +29,14 @@ public class EarthEditor : MonoBehaviour
     {
         earthMaterial.SetTexture("_TectonicTexture", currentTimelineToEdit.tectonicMap);
         earthMaterial.SetTexture("_LandmassTexture", currentTimelineToEdit.landmassMap);
+        if (currentEditingState == EarthEditorState.Tectonics)
+        {
+            earthMaterial.SetInt("_ViewTectonics", 1);
+        }
+        else
+        {
+            earthMaterial.SetInt("_ViewTectonics", 0);
+        }
+
     }
 }
